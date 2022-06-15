953fc25ee40e334cb5a7b400f15cbf8353a95abd
Wilson Santos de OLiveira <w.s.oliveira12@hotmail.com>
2022-06-15 19:39:16 -0300

    criação dos componentes

---
+++ src/componentes/Filtro-component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function FiltroComp() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
---
+++ src/componentes/Header-component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function HeaderComp() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
