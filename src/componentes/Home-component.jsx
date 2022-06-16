bfe50750c5bfd97417c92bafd39538b29b0302ab
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-06-16 19:37:53 -0300

    Adicionando estrutura dos cards

--- src/componentes/Home-component.jsx
+++ src/componentes/Home-component.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import CardComponent from '../Exercicio/Exercicio'
+import styled from "styled-components";
 
-export function HomeComp() {
+const DivPricipal = styled.div`
+  display: grid;
+  grid-template-columns: 20% 20% 20% 20%;
+  `
+
+export default function HomeComp() {
   return (
-    <div>
-      
-    </div>
+    <DivPricipal>
+      <CardComponent
+      imagem='https://images-na.ssl-images-amazon.com/images/I/718VgOqXQsL.__AC_SX300_SY300_QL70_ML2_.jpg'
+      nome='Pedra Roxa'
+      descricao=' Descrição ou tipo?'
+      valor='65,55'
+      />
+      <CardComponent
+      imagem='https://images-na.ssl-images-amazon.com/images/I/718VgOqXQsL.__AC_SX300_SY300_QL70_ML2_.jpg'
+      nome='Pedra Roxa'
+      descricao='Isso nem é uma pedra espacial -.-'
+      valor='65,55'
+      />
+      <CardComponent
+      imagem='https://images-na.ssl-images-amazon.com/images/I/718VgOqXQsL.__AC_SX300_SY300_QL70_ML2_.jpg'
+      nome='Pedra Roxa'
+      descricao='Isso nem é uma pedra espacial -.-'
+      valor='65,55'
+      />
+      <CardComponent
+      imagem='https://images-na.ssl-images-amazon.com/images/I/718VgOqXQsL.__AC_SX300_SY300_QL70_ML2_.jpg'
+      nome='Pedra Roxa'
+      descricao='Isso nem é uma pedra espacial -.-'
+      valor='65,55'
+      />
+    </DivPricipal>
   );
 }
 
