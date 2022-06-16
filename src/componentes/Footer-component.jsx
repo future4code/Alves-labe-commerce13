60f835ff274a65dbee5ca7a304a3b6fef8619b1d
Wilson Santos de OLiveira <w.s.oliveira12@hotmail.com>
2022-06-15 22:00:03 -0300

    Header,home e Footter

--- src/componentes/Footer-component.jsx
+++ src/componentes/Footer-component.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+const FooterStyled=styled.footer`
+width: 100%;
+height: 20vh;
+background-color: #2ea52a;
+`
+
+
 
 
 export function FooterComp() {
   return (
-    <div>
-      
-    </div>
+    <FooterStyled>
+      oiii eu sou o footter
+    </FooterStyled>
   );
 }
 
--- src/componentes/Home-component.jsx
+++ src/componentes/Home-component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+const Homerstyled=styled.main`
+  width: 100%;
+height: 100vh;
+background-color: #7f2a99;
+
+`
+
 export function HomeComp() {
   return (
-    <div>
-      
-    </div>
+    <Homerstyled>
+      eu sou o home
+    </Homerstyled>
   );
 }
 
